5475bd1965ac6d52ff867628eabffe50691c85e6
nicolassantos-935 <nicolas.santos@dcomp.ufs.br>
2026-07-03 10:29:38 -0300

    Arquivo main, responsável pela execução de todo o código

---
+++ projetopoo/__main__.py
@@ -0,0 +1,10 @@
+from tk import PaintApp
+
+# Função que irá executar todo o código
+def main():
+    app = PaintApp()
+    app.executar()
+
+# Parte final da modularização, que checa o nome do arquivo e, caso receba True, executa a função main
+if __name__ == "__main__":
+    main()
